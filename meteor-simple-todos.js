60872f3f9b22735a017f1006de63d18129ac2967
tony2cssc <jijing.guo@gmail.com>
2016-02-18 00:16:46 +0800

    Checking off and deleting tasks

--- meteor-simple-todos.js
+++ meteor-simple-todos.js
@@ -1,29 +1,41 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+    // This code only runs on the client
+    Template.body.helpers({
+        tasks: function() {
+            return Tasks.find({}, { sort: { createdAt: -1 } });
+        }
+    });
 
-  Template.body.events({
-    "submit.new-tasks": function(event) {
-      // Prevent default browser from submit
-      event.preventDefault();
+    Template.body.events({
+        "submit.new-tasks": function(event) {
+            // Prevent default browser from submit
+            event.preventDefault();
 
-      // Get value from form element
-      var text = event.target.text.value;
+            // Get value from form element
+            var text = event.target.text.value;
 
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date()
-      });
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
 
-      //Clear from
-      event.target.text.value = "";
-    }
-  });
+            //Clear from
+            event.target.text.value = "";
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function() {
+            Tasks.update(this._id, {
+                $set: { checked: !this.checked }
+            });
+        },
+        "click .delete": function() {
+            Tasks.remove(this._id);
+        }
+
+    });
 }
