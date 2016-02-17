7a05d274342fc157a9ec3ca881661cfe8bbae24f
Jijing Guo <jijing.guo@gmail.com>
2016-02-17 23:47:55 +0800

    Adding tasks with a form

--- meteor-simple-todos.js
+++ meteor-simple-todos.js
@@ -4,7 +4,26 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit.new-tasks": function(event) {
+      // Prevent default browser from submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //Clear from
+      event.target.text.value = "";
     }
   });
 }
